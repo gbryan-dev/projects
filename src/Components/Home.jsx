7c0f79168053f31792cff3f531e164c20b71a116
gbryan-dev <bryangalamgam@gmail.com>
2023-03-25 00:04:31 -0700

    .

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -68,6 +68,9 @@ const Home = () => {
 
                       <Link
                         type="button"
+                        target={
+                          proj.path.includes("gbryan-dev") ? "_blank" : ""
+                        }
                         to={proj.path}
                         onClick={() => new Audio(Sparkle).play()}
                         class="text-white h-[40px] float-right flex w-[120px] bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
